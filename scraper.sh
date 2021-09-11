fa7cf6b08a8eda3fd571aea03cc9e019f3c03fff
hudson smith <>
2021-09-11 16:08:01 -0500

    Finishing version one of scraper.sh.

--- scraper.sh
+++ scraper.sh
@@ -21,7 +21,7 @@ colors=(
 	["red"]="\033[31m"
 	["blue"]="\033[34m"
 	["green"]="\033[32m"
-	["purple"]"\033[35m"
+	["purple"]="\033[35m"
 	["brown"]="\033[33m"
 	["yellow"]="\033[1;33m"
 	["cyan"]="\033[36m"
@@ -36,37 +36,57 @@ main () {
 	# Check if the user inputs a web url,
 	# If not, then show them how to use the program.
 	if [ ! $ADDRESS ]; then
-		echo "Scraper.sh: Missing parameter \$1"
+		echo -e "${colors['red']}Scraper.sh: Missing parameter${colors['reset']} ${colors['cyan']}\$1${colors['reset']}"
 		usage
 		exit
 	fi
 	
 	# Get the web server, DNS addresses, and IP address of the server that replied.
-	SERVER=$(curl -I $ADDRESS | grep "Server" | awk '{print $2}')
-	DNS_LIST="${colors['cyan']}$(dig www.google.com +noall +answer | awk '{print $5}')${colors['reset']}"
+	SERVER_TYPE=$(curl -I $ADDRESS | grep "Server" | awk '{print $2}')
+	DNS_LIST="${colors['cyan']}$(dig $ADDRESS +noall +answer | awk '{print $5}')${colors['reset']}"
+	SERVER=$(dig $ADDRESS +noall +stats | grep "SERVER" | awk '{print $3}' | sed 's/\([^\(\)]+\)//g')
+	echo $SERVER
 
 	# Clear the screen before printing the information.
 	clear
 
+	# Print "scraper.sh" in ascii art.
+	echo -e "
+${colors["cyan"]}                                           _     
+                                          | |    
+  ___  ___ _ __ __ _ _ __   ___ _ __   ___| |__  
+ / __|/ __| '__/ _\` | '_ \ / _ \ '__| / __| '_ \ 
+ \__ \ (__| | | (_| | |_) |  __/ |_   \__ \ | | |
+ |___/\___|_|  \__,_| .__/ \___|_(_)  |___/_| |_|
+                    | |                          
+                    |_|                          
+	"
+
 	echo -e "
 ${colors["green"]}Address:${colors["reset"]} ${colors["cyan"]}$ADDRESS${colors["reset"]}
 
-${colors["green"]}Web Server:${colors["reset"]} ${colors["cyan"]}$SERVER${colors["reset"]}
+${colors["green"]}Web Server:${colors["reset"]} ${colors["cyan"]}$SERVER_TYPE${colors["reset"]}
 
 ${colors["green"]}DNS Lookup for${colors["reset"]} ${colors["cyan"]}$ADDRESS${colors["reset"]}${colors["green"]}${colors["cyan"]}:${colors["reset"]}
 $DNS_LIST
+
+${colors["green"]}Server and port${colors["reset"]}
+${colors["cyan"]}$SERVER${colors["reset"]}
 	"
 }
 
 usage () {
-	echo "
-	Scraper.sh -- Usage:
+	echo -e "
+${colors["red"]}Scraper.sh -- Usage:${colors["reset"]}
 
-		scraper.sh <param>
-			@param must be:
-				- Web url,
-				- --help
+scraper.sh ${colors["cyan"]}<param>${colors["reset"]}
+${colors["cyan"]}@param${colors["reset"]} must be:
+${colors["green"]}	- Web url,
+	- --help${colors["reset"]}
 	"
 }	
 
 main
+
+# Echo out info about the creator.
+echo -e "${colors["cyan"]}Made by @hudsonsmith on github! :)${colors["reset"]}"
